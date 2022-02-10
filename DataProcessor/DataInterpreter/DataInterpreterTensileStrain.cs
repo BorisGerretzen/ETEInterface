1f9760504390fb9327eed2c92c2f3cbc0bc1f608
Boris Gerretzen <borisgerretzen@gmail.com>
2022-02-10 04:07:46 +0100

    Revert "Revert "Add project files.""
    This reverts commit 9d6cf04fcff65ee972c5140b8e97a0b812fe565b.

---
+++ DataProcessor/DataInterpreter/DataInterpreterTensileStrain.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MoreLinq;
+using OfficeOpenXml;
+
+namespace DataProcessor {
+    public class DataInterpreterTensileStrain : AbstractDataInterpreter{
+        private List<(double, double)> _peaks;
+
+        public DataInterpreterTensileStrain(List<List<(double, double)>> data) : base(data) { }
+
+        public override List<string> GetHeaders() {
+            return new List<string> {
+                "min",
+                "mean",
+                "max",
+                "error under",
+                "error over"
+            };
+        }
+
+        public override List<double> GetData() {
+            return new List<double> {
+                GetMinPeak(),
+                GetMeanPeak(),
+                GetMaxPeak(),
+                GetMeanPeak() - GetMinPeak(),
+                GetMaxPeak() - GetMeanPeak()
+            };
+        }
+
+        private List<(double, double)> GetPeaks() {
+            if (_peaks == null) {
+                _peaks = _data.Select(sample => sample.MaxBy(x => x.Item2).First()).ToList();
+            }
+            return _peaks;
+        }
+
+        private double GetMeanPeak() {
+            return GetPeaks().Average(peak => peak.Item2);
+        }
+
+        private double GetMinPeak() {
+            return GetPeaks().Min(peak => peak.Item2);
+        }
+
+        private double GetMaxPeak() {
+            return GetPeaks().Max(peak => peak.Item2);
+        }
+    }
+}
---
+++ DataProcessor/DataProcessor/TensileProcessor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataProcessor.DataProcessor {
+    public class TensileProcessor : AbstractProcessor {
+        public TensileProcessor(string directory, bool separate) : base(directory, separate) {
+            _filter = "*.xlsx";
+        }
+
+        public override void Process(string outputFile) {
+            List<(string, List<double>)> dataStrain = new List<(string, List<double>)>();
+            List<(string, List<double>)> dataElongation = new List<(string, List<double>)>();
+            List<string> headers = new List<string>();
+
+            foreach (string file in Directory.GetFiles(_directory,_filter)) {
+                AbstractDataReader reader = new DataReaderTensile(file, "Values Series");
+                AbstractDataInterpreter interpreterStrain = new DataInterpreterTensileStrain(reader.ReadData());
+                AbstractDataInterpreter interpreterElongation = new DataInterpreterTensileElongation(reader.ReadData());
+                var rowStrain = (Path.GetFileNameWithoutExtension(file), interpreterStrain.GetData());
+                var rowElongation = (Path.GetFileNameWithoutExtension(file), interpreterElongation.GetData());
+                dataStrain.Add(rowStrain);
+                dataElongation.Add(rowElongation);
+                headers = interpreterStrain.GetHeaders();
+            }
+
+            DataWriter writer = new DataWriter(dataStrain, headers);
+            writer.Write(outputFile, "TensileStrain", _separate);
+            writer = new DataWriter(dataElongation, headers);
+            writer.Write(outputFile, "TensileElongation", _separate);
+        }
+    }
+}
